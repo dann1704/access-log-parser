65a8f47f3195f6912ba9c80096d394d251e1aa23
Danil Mironov <daniilivmironov@gmail.com>
2024-07-14 18:14:41 +0300

    Добавлен код, получающий последовательно два числа из консоли, а затем вычисляющий и выводящий в консоль сумму, разность, произведение и частное этих чисел

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner (System.in) .nextInt ();
+
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner (System.in) .nextInt ();
+
+        int sum = number1 + number2;
+        int dif = number1 - number2;
+        int multi = number1 * number2;
+        double quotient =  (double) number1 / number2;
+
+        System.out.println("Сумма:" + sum);
+        System.out.println("Разность:" + dif);
+        System.out.println("Произведение:" + multi);
+        System.out.println("Частное:" + quotient);
     }
 }
